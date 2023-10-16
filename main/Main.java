4a882cd3e2035c8429a65ee408f3a747fef4eaff
steveagustinus <freuchsteve@gmail.com>
2023-10-16 10:11:32 +0700

    add feature New Cinema

--- main/Main.java
+++ main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
 import controller.Controller;
+import view.admin.NewCinemaScreen;
 
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.getCinemaList();
+
+        new NewCinemaScreen();
     }
 }

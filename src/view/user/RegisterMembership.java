fcddada18ffc8252a78e9a282bf86ce8bafae5c0
Irvandmw <irvandmw@gmail.com>
2023-11-22 17:10:50 +0700

    Update RegisterMembership

--- src/view/user/RegisterMembership.java
+++ src/view/user/RegisterMembership.java
@@ -12,9 +12,13 @@ public class RegisterMembership {
         regisMembership();
     }
 
+    public static void main(String[] args) {
+        new RegisterMembership();
+    }
+
     private void regisMembership(){
         Controller controller = new Controller();
-        boolean checkStatus = controller.checkMembership(UserDataSingleton.getInstance().getUsername());
+        boolean checkStatus = false; //controller.checkMembership(UserDataSingleton.getInstance().getUsername());
 
         if(checkStatus == true){
             JOptionPane.showMessageDialog(null, "Anda sudah terdaftar menjadi member, kembali ke menu", "", JOptionPane.INFORMATION_MESSAGE);
@@ -24,11 +28,14 @@ public class RegisterMembership {
             f.setTitle("Menu Register");
             f.setSize(720, 480);
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setLocationRelativeTo(null);
+            
 
             JButton buttonRegis = new JButton("Register Membership");
             buttonRegis.setBounds(170, 220, 150, 50);
-            f.add(buttonRegis);
+
+            JButton buttonBack = new JButton("< Back to Main Menu");
+            buttonBack.setBounds(170, 290, 150, 50);
+            
             buttonRegis.addActionListener(e -> {
                 controller.registerMembership(UserDataSingleton.getInstance().getUsername(), UserDataSingleton.getInstance().getPassword(), UserDataSingleton.getInstance().getPassword(), UserDataSingleton.getInstance().getEmail(), UserDataSingleton.getInstance().getPhone_no(), UserDataSingleton.getInstance().getAddress(), 0);
                 JOptionPane.showMessageDialog(null, "Anda sudah terdaftar menjadi member, kembali ke menu", "", JOptionPane.INFORMATION_MESSAGE);
@@ -36,13 +43,15 @@ public class RegisterMembership {
                 new MainMenuUserScreen();
             });
 
-            JButton buttonBack = new JButton("< Back to Main Menu");
-            buttonBack.setBounds(170, 290, 150, 50);
-            f.add(buttonBack);
             buttonBack.addActionListener(e -> {
                 new MainMenuUserScreen();
                 f.dispose();
             });
+            f.add(buttonRegis);
+            f.add(buttonBack);
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+            
         }
     }
 }

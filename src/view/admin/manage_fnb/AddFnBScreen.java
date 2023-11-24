97960cd49dc6e02f6c4ee0c62328eb255c20ff9d
MrS2111 <stevenhalim2111@gmail.com>
2023-11-24 17:29:44 +0700

    update screen manage fnb

--- src/view/admin/manage_fnb/AddFnBScreen.java
+++ src/view/admin/manage_fnb/AddFnBScreen.java
@@ -13,33 +13,128 @@ public class AddFnBScreen {
         Controller controller = new Controller();
         JFrame frame = new JFrame();
         frame.setTitle("Add FnB");
-        frame.setSize(400,200);
-        JTextField namaFnB = new JTextField(40);
-        JTextField harga = new JTextField(40);;
-        JTextArea description= new JTextArea();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        // JTextField namaFnB = new JTextField(40);
+        // JTextField harga = new JTextField(40);;
+        // JTextArea description= new JTextArea();
+        // JPanel panel = new JPanel();
+        // panel.setLayout(new GridLayout(4,2));
+        // JLabel namaFnBlabel = new JLabel("Nama FnB : ");
+        // JLabel hargaFnBlabel = new JLabel("Harga FnB : ");
+        // JLabel descriptionFnBlabel = new JLabel("Deskripsi FnB : ");
+        // JButton submitButton = new JButton("Submit");
+        // submitButton.setSize(100,50);
+
+        // panel.add(namaFnBlabel);
+        // panel.add(namaFnB);
+
+        // panel.add(hargaFnBlabel);
+        // panel.add(harga);
+
+        // panel.add(descriptionFnBlabel);
+        // panel.add(description);
+        
+        // panel.add(new JLabel());
+        // panel.add(submitButton);
+        
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4,2));
+        panel.setLayout(null);
+
+        JLabel mainLabel = new JLabel("Add Food and Beverages");
+        mainLabel.setBounds(70, 10, 350, 30);
+        mainLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        panel.add(mainLabel);
+
+
         JLabel namaFnBlabel = new JLabel("Nama FnB : ");
-        JLabel hargaFnBlabel = new JLabel("Harga FnB : ");
-        JLabel descriptionFnBlabel = new JLabel("Deskripsi FnB : ");
-        JButton submitButton = new JButton("Submit");
-        submitButton.setSize(100,50);
-
+        namaFnBlabel.setBounds(10, 50, 100, 30);
         panel.add(namaFnBlabel);
+
+        JTextField namaFnB = new JTextField(40);
+        namaFnB.setBounds(120, 50, 200, 30);
+        namaFnB.setText("Enter FnB Name");
+        namaFnB.setForeground(Color.GRAY);
         panel.add(namaFnB);
+        namaFnB.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                if (namaFnB.getText().equals("Enter FnB Name")) {
+                    namaFnB.setText("");
+                    namaFnB.setForeground(Color.BLACK);
+                }
+            }
 
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                if (namaFnB.getText().equals("")) {
+                    namaFnB.setText("Enter FnB Name");
+                    namaFnB.setForeground(Color.GRAY);
+                }
+            }
+        });
+
+        JLabel hargaFnBlabel = new JLabel("Harga FnB : ");
+        hargaFnBlabel.setBounds(10, 100, 100, 30);
         panel.add(hargaFnBlabel);
+
+        JTextField harga = new JTextField(40);
+        harga.setBounds(120, 100, 200, 30);
+        harga.setText("Enter FnB Price");
+        harga.setForeground(Color.GRAY);
         panel.add(harga);
+        harga.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                if (harga.getText().equals("Enter FnB Price")) {
+                    harga.setText("");
+                    harga.setForeground(Color.BLACK);
+                }
+            }
 
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                if (harga.getText().equals("")) {
+                    harga.setText("Enter FnB Price");
+                    harga.setForeground(Color.GRAY);
+                }
+            }
+        });
+
+        JLabel descriptionFnBlabel = new JLabel("Deskripsi FnB : ");
+        descriptionFnBlabel.setBounds(10, 150, 100, 30);
         panel.add(descriptionFnBlabel);
+
+        JTextArea description= new JTextArea();
+        description.setBounds(120, 150, 200, 100);
+        description.setText("Enter FnB Description");
+        description.setForeground(Color.GRAY);
         panel.add(description);
+        description.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                if (description.getText().equals("Enter FnB Description")) {
+                    description.setText("");
+                    description.setForeground(Color.BLACK);
+                }
+            }
 
-        panel.add(new JLabel());
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                if (description.getText().equals("")) {
+                    description.setText("Enter FnB Description");
+                    description.setForeground(Color.GRAY);
+                }
+            }
+        });
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(30, 270, 100, 30);
+        panel.add(backButton);
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                // new ManageFnBScreen();
+            }
+        });
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(200, 270, 100, 30);
         panel.add(submitButton);
-
-        frame.add(panel);
-        frame.setVisible(true);
-
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,5 +162,12 @@ public class AddFnBScreen {
             }
         });
 
+
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        new AddFnBScreen();
     }
 }
--- src/view/admin/manage_fnb/EditFnBScreen.java
+++ src/view/admin/manage_fnb/EditFnBScreen.java
@@ -1,97 +1,203 @@
 package src.view.admin.manage_fnb;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import src.controller.Controller;
 import src.controller.OperationCode;
+import src.model.FnB;
 import src.view.admin.MainMenuScreen;
+
 public class EditFnBScreen {
-    public  EditFnBScreen(){
+    public EditFnBScreen() {
         Controller controller = new Controller();
         JFrame frame = new JFrame();
-        frame.setTitle("Edit FnB");
-        frame.setSize(400,200);
+        frame.setTitle("Edit and Delete FnB");
+        frame.setSize(400, 600);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6,2));
+        panel.setLayout(null);
+
+        JLabel mainLabel = new JLabel("Edit and Delete Food and Beverages");
+        mainLabel.setBounds(10, 10, 350, 30);
+        mainLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        panel.add(mainLabel);
+
         JLabel namaFnB = new JLabel("Nama-nama Food and Beverages : ");
-        JLabel namaFnBlabel = new JLabel("Update nama FnB : ");
-        JLabel hargaFnBlabel = new JLabel("Update Harga FnB : ");
-        JLabel descriptionFnBlabel = new JLabel("Update Deskripsi FnB : ");
-        String [] namaFoodAndBevs = controller.listFNB();
-        JComboBox<String> foodsnbevs = new JComboBox<>(namaFoodAndBevs);
-        JTextField nama = new JTextField(20);
-        JTextField harga = new JTextField(20);;
-        JTextArea description= new JTextArea();
-        JButton button = new JButton("Edit FnB");
-
-        foodsnbevs.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                nama.removeAll();
-                harga.removeAll();
-                description.removeAll();
-                nama.setText(controller.getFnBbyName(foodsnbevs.getSelectedItem().toString()).getNama());
-                harga.setText(String.valueOf(controller.getFnBbyName(foodsnbevs.getSelectedItem().toString()).getHarga()));
-                description.setText(controller.getFnBbyName(foodsnbevs.getSelectedItem().toString()).getDescription());
-            }
-
-        });
-
+        namaFnB.setBounds(10, 50, 350, 30);
         panel.add(namaFnB);
-        panel.add(foodsnbevs);
-        //Nama
-        panel.add(namaFnBlabel);
-        panel.add((nama));
 
-        //Harga
+        String[] namaFoodAndBevs = controller.listFNB();
+        JComboBox<String> foodsnbevs = new JComboBox<>(namaFoodAndBevs);
+        foodsnbevs.setBounds(10, 120, 350, 30);
+        panel.add(foodsnbevs);
+        // JTextField searchFnB = new JTextField(20);
+        // searchFnB.setBounds(10, 80, 350, 30);
+        // panel.add(searchFnB);
+        // searchFnB.getDocument().addDocumentListener(new DocumentListener() {
+        //     @Override
+        //     public void insertUpdate(DocumentEvent e) {
+        //         filterComboBox();
+        //     }
+
+        //     @Override
+        //     public void removeUpdate(DocumentEvent e) {
+        //         filterComboBox();
+        //     }
+
+        //     @Override
+        //     public void changedUpdate(DocumentEvent e) {
+        //         filterComboBox();
+        //     }
+
+        //     private void filterComboBox() {
+        //         String filter = searchFnB.getText();
+        //         DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>) foodsnbevs.getModel();
+        //         model.removeAllElements();
+        //         if (!filter.isEmpty()) {
+        //             for (String s : namaFoodAndBevs) {
+        //                 if (s.toLowerCase().contains(filter.toLowerCase())) {
+        //                     model.addElement(s);
+        //                 }
+        //             }
+        //         } else { 
+        //             for (String s : namaFoodAndBevs) {
+        //                 model.addElement(s);
+        //             }
+        //         }
+        //     }
+        // });
+
+        JRadioButton edit = new JRadioButton("Edit");
+        edit.setBounds(10, 160, 100, 30);
+        panel.add(edit);
+        JRadioButton delete = new JRadioButton("Delete");
+        delete.setBounds(110, 160, 100, 30);
+        delete.setSelected(true);
+        panel.add(delete);
+        ButtonGroup group = new ButtonGroup();
+        group.add(edit);
+        group.add(delete);
+
+        JLabel namaFnBlabel = new JLabel("Nama : ");
+        namaFnBlabel.setBounds(10, 200, 100, 30);
+        namaFnBlabel.setVisible(false);
+        panel.add(namaFnBlabel);
+
+        JTextField nama = new JTextField(20);
+        nama.setBounds(120, 200, 200, 30);
+        nama.setVisible(false);
+        panel.add(nama);
+
+        JLabel hargaFnBlabel = new JLabel("Harga : ");
+        hargaFnBlabel.setBounds(10, 240, 100, 30);
+        hargaFnBlabel.setVisible(false);
         panel.add(hargaFnBlabel);
+
+        JTextField harga = new JTextField(20);
+        harga.setBounds(120, 240, 200, 30);
+        harga.setVisible(false);
         panel.add(harga);
 
-        //Deskripsi
+        JLabel descriptionFnBlabel = new JLabel("Deskripsi : ");
+        descriptionFnBlabel.setBounds(10, 280, 100, 30);
+        descriptionFnBlabel.setVisible(false);
         panel.add(descriptionFnBlabel);
+
+        JTextArea description = new JTextArea();
+        description.setBounds(120, 280, 200, 70);
+        description.setVisible(false);
         panel.add(description);
 
-        //Button
-        panel.add(new JLabel());
-        panel.add(button);
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(10, 350, 100, 30);
+        panel.add(backButton);
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                // new MainMenuScreen();
+            }
+        });
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(220, 350, 100, 30);
+        panel.add(submitButton);
+
+        foodsnbevs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                FnB fnb = controller.getFnBbyName(foodsnbevs.getSelectedItem().toString());
+                // searchFnB.setText(fnb.getNama());
+                nama.setText(fnb.getNama());
+                harga.setText(String.valueOf(fnb.getHarga()));
+                description.setText(fnb.getDescription());
+            }
+        });
+
+        edit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (edit.isSelected()) {
+                    namaFnBlabel.setVisible(true);
+                    hargaFnBlabel.setVisible(true);
+                    descriptionFnBlabel.setVisible(true);
+                    nama.setVisible(true);
+                    harga.setVisible(true);
+                    description.setVisible(true);
+                } else {
+                    namaFnBlabel.setVisible(false);
+                    hargaFnBlabel.setVisible(false);
+                    descriptionFnBlabel.setVisible(false);
+                    nama.setVisible(false);
+                    harga.setVisible(false);
+                    description.setVisible(false);
+                }
+            }
+        });
+        delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (delete.isSelected()) {
+                    namaFnBlabel.setVisible(false);
+                    hargaFnBlabel.setVisible(false);
+                    descriptionFnBlabel.setVisible(false);
+                    nama.setVisible(false);
+                    harga.setVisible(false);
+                    description.setVisible(false);
+                } else {
+                    namaFnBlabel.setVisible(true);
+                    hargaFnBlabel.setVisible(true);
+                    descriptionFnBlabel.setVisible(true);
+                    nama.setVisible(true);
+                    harga.setVisible(true);
+                    description.setVisible(true);
+                }
+            }
+        });
+        submitButton.addActionListener(e -> {
+            if(delete.isSelected()){
+                controller.deleteFnB(foodsnbevs.getSelectedItem().toString());
+                // new MainMenuScreen();
+            }else if(edit.isSelected()){
+                FnB fnb = controller.getFnBbyName(foodsnbevs.getSelectedItem().toString());
+                String[] tempFnB = { fnb.getNama() , String.valueOf(fnb.getHarga()) , fnb.getDescription()};
+                controller.editFnB(foodsnbevs.getSelectedItem().toString(), tempFnB);
+                new MainMenuScreen();
+            }
+        });
 
         frame.add(panel);
         frame.setVisible(true);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String oldNameFnB = foodsnbevs.getSelectedItem().toString();
-                String [] data = new String[3];
-                data[0]=nama.getText();
-                data[1]=harga.getText();
-                data[2]=description.getText();
-                int konfirmasi = controller.editFnB(oldNameFnB,data);
-                String status="";
-                if(konfirmasi==OperationCode.AddFnB.SUCCESS){
-                    status="Berhasil!";
-                    JOptionPane.showMessageDialog(null,status);
-                    frame.dispose();
-                    new MainMenuScreen();
-                }else if(konfirmasi==OperationCode.EditFnB.EMPTYNAME){
-                    status="Nama Kosong!";
-                }else if (konfirmasi==OperationCode.EditFnB.EMPTYHARGA){
-                    status="Harga Kosong!";
-                }else if(konfirmasi==OperationCode.EditFnB.EMPTYDESCRIPTION){
-                    status="Deskripsi Kosong!";
-                }else if(konfirmasi==OperationCode.EditFnB.ANYEXCEPTION){
-                    status="Error!";
-                }
-                
-                 JOptionPane.showMessageDialog(null,status);
-                frame.dispose();
-            }
-        });
+        frame.setLocationRelativeTo(null);
 
     }
 
+    public static void main(String[] args) {
+        new EditFnBScreen();
+    }
 }

0bf80b134d49320eaff8ead2f0291edccaed1f82
steveagustinus <freuchsteve@gmail.com>
2023-11-24 18:05:51 +0700

    register screen add theme

--- src/view/MainInterface.java
+++ src/view/MainInterface.java
@@ -8,16 +8,16 @@ public interface MainInterface {
     public final static JFrame mainFrame = new JFrame("Biskop Hub");
 
     // Bioskop hub
-    // public final static Color FRAME_BACKGROUND = new Color(20, 20, 20);
-    // public final static Color TEXT_BACKGROUND = Color.WHITE;
-    // public final static Color BUTTON_BACKGROUND = new Color(239, 146, 28);
-    // public final static Color BUTTON_FOREGROUND = new Color(20, 20, 20);
-    // public final static String FONTFAMILY = "Arial";
+    public final static Color FRAME_BACKGROUND = new Color(20, 20, 20);
+    public final static Color TEXT_BACKGROUND = Color.WHITE;
+    public final static Color BUTTON_BACKGROUND = new Color(239, 146, 28);
+    public final static Color BUTTON_FOREGROUND = new Color(20, 20, 20);
+    public final static String FONTFAMILY = "Arial";
 
     // Bioskop hub
-    public final static Color FRAME_BACKGROUND = new Color(207,229,235);
-    public final static Color TEXT_BACKGROUND = Color.BLACK;
-    public final static Color BUTTON_BACKGROUND = new Color(0, 100, 219);
-    public final static Color BUTTON_FOREGROUND = Color.WHITE;
-    public final static String FONTFAMILY = "Arial";
+    // public final static Color FRAME_BACKGROUND = new Color(207,229,235);
+    // public final static Color TEXT_BACKGROUND = Color.BLACK;
+    // public final static Color BUTTON_BACKGROUND = new Color(0, 100, 219);
+    // public final static Color BUTTON_FOREGROUND = Color.WHITE;
+    // public final static String FONTFAMILY = "Arial";
 }
--- src/view/RegisterScreen.java
+++ src/view/RegisterScreen.java
@@ -9,29 +9,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RegisterScreen {
+public class RegisterScreen implements MainInterface {
     JFrame frame = new JFrame();
     Controller controller = new Controller();
 
     public RegisterScreen() {
         frame.setTitle("Register");
+        frame.setBackground(FRAME_BACKGROUND);
 
         JLabel mainlabel = new JLabel("Register to Bioskop HUB!");
         mainlabel.setBounds(30, 10, 500, 50);
         mainlabel.setFont(new Font("Arial", Font.BOLD, 18));
+        mainlabel.setForeground(TEXT_BACKGROUND);
 
         JLabel usernameLabel = new JLabel("Username");
         usernameLabel.setBounds(20, 50, 80, 25);
+        usernameLabel.setForeground(TEXT_BACKGROUND);
 
         JTextField usernameField = new JTextField(20);
         usernameField.setBounds(20, 70, 230, 25);
         usernameField.setText("Enter your Username");
+        usernameField.setBackground(FRAME_BACKGROUND);
         usernameField.setForeground(Color.GRAY);
         usernameField.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 if (usernameField.getText().equals("Enter your Username")) {
                     usernameField.setText("");
-                    usernameField.setForeground(Color.BLACK);
+                    usernameField.setForeground(TEXT_BACKGROUND);
                 }
             }
 
@@ -45,53 +49,60 @@ public class RegisterScreen {
 
         JLabel passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(20, 90, 80, 25);
+        passwordLabel.setForeground(TEXT_BACKGROUND);
 
         JPasswordField passwordField = new JPasswordField(20);
         passwordField.setBounds(20, 110, 230, 25);
+        passwordField.setBackground(FRAME_BACKGROUND);
         controller.setPlaceholder(passwordField, "Enter your Password");
 
         JLabel confirmPasswordLabel = new JLabel("Confirm Password");
         confirmPasswordLabel.setBounds(20, 130, 160, 25);
+        confirmPasswordLabel.setForeground(TEXT_BACKGROUND);
 
         JPasswordField confirmPasswordField = new JPasswordField(20);
         confirmPasswordField.setBounds(20, 150, 230, 25);
+        confirmPasswordField.setBackground(FRAME_BACKGROUND);
         controller.setPlaceholder(confirmPasswordField, "Re-enter your Password");
 
         JLabel emailLabel = new JLabel("Email");
         emailLabel.setBounds(20, 170, 80, 25);
+        emailLabel.setForeground(TEXT_BACKGROUND);
 
         JTextField emailField = new JTextField(20);
         emailField.setBounds(20, 190, 230, 25);
         emailField.setText("Enter your Email");
-        emailField.setForeground(Color.GRAY);
+        emailField.setBackground(FRAME_BACKGROUND);
         emailField.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 if (emailField.getText().equals("Enter your Email")) {
                     emailField.setText("");
-                    emailField.setForeground(Color.BLACK);
+                    emailField.setForeground(TEXT_BACKGROUND);
                 }
             }
 
             public void focusLost(java.awt.event.FocusEvent evt) {
                 if (emailField.getText().equals("")) {
                     emailField.setText("Enter your Email");
-                    emailField.setForeground(Color.GRAY);
+                    emailField.setForeground(TEXT_BACKGROUND);
                 }
             }
         });
 
         JLabel phoneNumLabel = new JLabel("Phone Number");
         phoneNumLabel.setBounds(20, 210, 120, 25);
+        phoneNumLabel.setForeground(TEXT_BACKGROUND);
 
         JTextField phoneNumField = new JTextField(20);
         phoneNumField.setBounds(20, 230, 230, 25);
         phoneNumField.setText("Enter your Phone Number");
+        phoneNumField.setBackground(FRAME_BACKGROUND);
         phoneNumField.setForeground(Color.GRAY);
         phoneNumField.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 if (phoneNumField.getText().equals("Enter your Phone Number")) {
                     phoneNumField.setText("");
-                    phoneNumField.setForeground(Color.BLACK);
+                    phoneNumField.setForeground(TEXT_BACKGROUND);
                 }
             }
 
@@ -105,16 +116,18 @@ public class RegisterScreen {
 
         JLabel addressLabel = new JLabel("Address");
         addressLabel.setBounds(20, 250, 80, 25);
+        addressLabel.setForeground(TEXT_BACKGROUND);
 
         JTextField addressField = new JTextField(20);
         addressField.setBounds(20, 270, 230, 25);
         addressField.setText("Enter your Address");
+        addressField.setBackground(FRAME_BACKGROUND);
         addressField.setForeground(Color.GRAY);
         addressField.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 if (addressField.getText().equals("Enter your Address")) {
                     addressField.setText("");
-                    addressField.setForeground(Color.BLACK);
+                    addressField.setForeground(TEXT_BACKGROUND);
                 }
             }
 
@@ -127,13 +140,18 @@ public class RegisterScreen {
         });
 
         JButton registerButton = new JButton("Register");
+        registerButton.setBackground(BUTTON_BACKGROUND);
+        registerButton.setForeground(BUTTON_FOREGROUND);
         registerButton.setBounds(20, 320, 230, 25);
 
         JLabel loginLabel = new JLabel("Already have an account?");
         loginLabel.setBounds(60, 350, 150, 25);
+        loginLabel.setForeground(TEXT_BACKGROUND);
 
         JButton loginButton = new JButton("Login");
         loginButton.setBounds(20, 370, 230, 25);
+        loginButton.setBackground(BUTTON_BACKGROUND);
+        loginButton.setForeground(BUTTON_FOREGROUND);
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -143,6 +161,7 @@ public class RegisterScreen {
         });
 
         JPanel panel = new JPanel();
+        panel.setBackground(FRAME_BACKGROUND);
         panel.setLayout(null);
 
         panel.add(usernameLabel);
